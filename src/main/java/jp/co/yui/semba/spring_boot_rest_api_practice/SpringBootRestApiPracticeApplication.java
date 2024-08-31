52eb2dd021dc73c91dc1bb20d98597103f044fb8
Yui Semba <65758369+semba-yui@users.noreply.github.com>
2024-08-31 17:22:37 +0900

    feat: init spring initializr

---
+++ src/main/java/jp/co/yui/semba/spring_boot_rest_api_practice/SpringBootRestApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package jp.co.yui.semba.spring_boot_rest_api_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/yui/semba/spring_boot_rest_api_practice/SpringBootRestApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.yui.semba.spring_boot_rest_api_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
