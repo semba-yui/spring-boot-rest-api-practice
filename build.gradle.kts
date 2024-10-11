b88abde6b5aa98d6f298a229bbb82b696766e248
Yui Semba <65758369+semba-yui@users.noreply.github.com>
2024-10-11 20:34:20 +0900

    mod

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,60 +23,31 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2023.0.3"
-
-// TODO: Version Catalog
-val logbackEncoderVersion = "8.0"
-val logbookVersion = "3.9.0"
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
-    implementation("net.logstash.logback:logstash-logback-encoder:${logbackEncoderVersion}")
-    implementation("org.zalando:logbook-spring-boot-starter:${logbookVersion}")
-    compileOnly("org.projectlombok:lombok")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.security)
+    implementation(libs.spring.boot.starter.validation)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.cloud.starter.circuitbreaker.resilience4j)
+    implementation(libs.logstash.logback.encoder)
+    implementation(libs.logbook.spring.boot.starter)
+    compileOnly(libs.lombok)
+    runtimeOnly(libs.h2)
+    runtimeOnly(libs.postgresql)
+    annotationProcessor(libs.spring.boot.configuration.processor)
+    annotationProcessor(libs.lombok)
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.security.test)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${libs.versions.spring.cloud.dependencies.get()}")
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-// Git ハッシュを取得
-val gitCommit: String by lazy {
-    val process = ProcessBuilder("git", "rev-parse", "--short", "HEAD")
-        .redirectErrorStream(true)
-        .start()
-    process.inputStream.bufferedReader().use { it.readText().trim() }
-}
-
-tasks.named<Jar>("bootJar") {
-    manifest {
-        attributes(
-            "Git-Commit" to gitCommit,
-            "App-Version" to project.version
-        )
-    }
-}
-
-tasks.named<ProcessResources>("processResources") {
-    filesMatching("**/application.properties") {
-        expand("gitCommit" to gitCommit, "appVersion" to project.version)
-    }
 }
